02a7aa2cc3e5030179d3488e5307ea34332047e4
yardie <641233646@qq.com>
2025-08-04 22:54:53 +0800

    add 003led

---
+++ test/1st/8class/003led/main.c
@@ -0,0 +1,9 @@
+int main()
+{
+    unsigned int *pGPFCON = (unsigned int *)0x56000050;
+    unsigned int *pGPFDAT = (unsigned int *)0x56000054;
+
+    *pGPFCON = 0x100;
+    *pGPFDAT = 0;
+    return 0;
+}
