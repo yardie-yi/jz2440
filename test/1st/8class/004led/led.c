61b7270edbc1f31843a0f9d9b6fc7f8768eae258
yardie <641233646@qq.com>
2025-08-11 23:41:16 +0800

    add 005

---
+++ test/1st/8class/004led/led.c
@@ -0,0 +1,24 @@
+void delay(volatile int d)
+{
+    while (d--);
+}
+
+
+int led_on(int which)
+{
+    
+    unsigned int *pGPFCON = (unsigned int *)0x56000050;
+    unsigned int *pGPFDAT = (unsigned int *)0x56000054;
+    if (which == 4)
+    {
+        *pGPFCON = 0x100;
+    }
+    else if (which == 5)
+    {
+        *pGPFCON = 0x400;
+    }
+    *pGPFDAT = 0;
+    return 0;
+}
+
+
